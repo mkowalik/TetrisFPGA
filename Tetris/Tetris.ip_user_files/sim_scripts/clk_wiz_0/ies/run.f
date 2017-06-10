f998e0ff9d73254638ae5697f7cd3e3eb55f0fcc
Michał Kowalik <kowalik.michal1@gmail.com>
2017-06-10 17:24:27 +0200

    Adding new clock.

--- Tetris/Tetris.ip_user_files/sim_scripts/clk_wiz_0/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies/xil_defaultlib \
-  "../../../../Tetris.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
-  "../../../../Tetris.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
--endlib
--makelib ies/xil_defaultlib \
-  glbl.v
--endlib
-
